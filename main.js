87d672e2132e25fa496cc07fc4cea944359f8573
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-01-27 19:31:41 -0800

    fix main

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-if(typeof require !== 'undefined' && require.main === module){
-    require('./inputs').run(process.argv.slice(2));
+if (typeof require !== 'undefined' && require.main === module) {
+    require('inputs').run(process.argv.slice(2));
+} else {
+    module.imports = require('inputs');
 }
-
