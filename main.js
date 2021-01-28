ad69493eff3413b3ab47569ae689fa1fb1b668a3
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-01-27 19:46:44 -0800

    fix exports

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if (typeof require !== 'undefined' && require.main === module) {
     require('inputs').run(process.argv.slice(2));
 } else {
-    module.imports = require('inputs');
+    module.exports = require('inputs');
 }
